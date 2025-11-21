fb06b64668835642dd9e4bb87af75a924fce78d1
nela0222 <mariidiazsanchez@gmail.com>
2025-11-21 10:55:59 -0500

    Agregue gettipo usuario a usuariocoordinador, ademas cree la interfaz de formularioCoordinador

---
+++ Proaula/src/UsuarioCoordinador.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author marii
+ */
+public class UsuarioCoordinador extends Usuario {
+    
+private String area;
+private String codigo;
+private String id;
+private String cargo;
+
+public UsuarioCoordinador() {
+    super ("", "", "", "");
+    this.area = "";
+    this.codigo = "";
+    this.id = "";
+    this.cargo = "";
+}
+
+public UsuarioCoordinador ( String nombre, String apellidos, String correo, String contraseña, String area, String codigo, String id, String cargo){
+    super (nombre,  apellidos,  correo, contraseña);
+    this.area = area;
+    this.codigo = codigo;
+    this.id = id;
+    this.cargo = cargo;
+}
+
+public String getArea(){
+    return area;
+}
+
+public void setArea (String area){
+    this.area = area;
+
+}
+
+public String getCodigo(){
+    return codigo;
+}
+
+public void setCodigo(){
+    this.codigo = codigo;
+}
+
+public String getId() {
+    return id;
+}
+
+public void setId(){
+    this.id=id;    
+}
+
+public String getCargo(){
+    return cargo;
+}
+
+public void setCargo(){
+    this.cargo = cargo;
+}
+}
+
+
